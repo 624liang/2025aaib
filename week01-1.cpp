bbdc2b15f99afdeab0828f0ba609982a882d682e
624liang <13750280@me.mcu.edu.tw>
2025-04-16 10:09:12 +0800

    Add files via upload

---
+++ week01-1.cpp
@@ -0,0 +1,16 @@
+///week01-1.cpp 瘋狂程設 兩數相加
+///下週考試考這題，不用寫藍色的中文註解
+#include <stdio.h>/// 檔名不能寫錯
+/// stdio 的意思是 標準的standard input/output
+///
+
+
+int main()
+{
+	int a, b;
+	scanf( "%d" , &a );
+	scanf( "%d" , &b );
+
+	printf("%d" , a+b );
+
+}
